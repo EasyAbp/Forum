a7431024b5005115be466e2c73cc31394e7ee282
gdlcf88 <gdlcf88@outlook.com>
2022-02-10 01:38:57 +0800

    Upgrade to ABP 5.1.3

--- test/EasyAbp.Forum.HttpApi.Client.ConsoleTestApp/ConsoleTestAppHostedService.cs
+++ test/EasyAbp.Forum.HttpApi.Client.ConsoleTestApp/ConsoleTestAppHostedService.cs
@@ -18,17 +18,17 @@ namespace EasyAbp.Forum.HttpApi.Client.ConsoleTestApp
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            using (var application = AbpApplicationFactory.Create<ForumConsoleApiClientModule>(options=>
+            using (var application = await AbpApplicationFactory.CreateAsync<ForumConsoleApiClientModule>(options=>
             {
                 options.Services.ReplaceConfiguration(_configuration);
             }))
             {
-                application.Initialize();
+                await application.InitializeAsync();
 
                 var demo = application.ServiceProvider.GetRequiredService<ClientDemoService>();
                 await demo.RunAsync();
 
-                application.Shutdown();
+                await application.ShutdownAsync();
             }
         }
 
